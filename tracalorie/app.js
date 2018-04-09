6fe416191489005c11746620486d883b7d23016a
anne <richardson.ae@gmail.com>
2018-04-09 11:47:39 -0500

    Updates item and total calories

--- tracalorie/app.js
+++ tracalorie/app.js
@@ -12,9 +12,9 @@ const ItemController = (function(){
 
   const data = {
     items: [
-      // {id: 0, name: "Steak dinner", calories: 1200},
-      // {id: 1, name: "Kraft dinner", calories: 500},
-      // {id: 2, name: "Pizza", calories: 200},
+      {id: 0, name: "Steak dinner", calories: 1200},
+      {id: 1, name: "Kraft dinner", calories: 500},
+      {id: 2, name: "Pizza", calories: 200},
       // {id: 3, name: "Cookie", calories: 400},
       // {id: 4, name: "Eggs", calories: 300}
     ],
@@ -50,6 +50,23 @@ const ItemController = (function(){
       })
       return found
     },
+    processCalorieInput: function(input){
+      let cal;
+      input === '' ? cal = 0 : cal = input
+      return parseInt(cal)
+    },
+    updateItemData: function(name, calories){
+      calories = ItemController.processCalorieInput(calories)
+      let found = null
+      data.items.forEach(function(item){
+        if(item.id === data.currentItem.id){
+          item.name = name
+          item.calories = calories
+          found = item
+        }
+      })
+      return found
+    },
     setCurrentItem: function(item){
       data.currentItem = item
     },
@@ -74,6 +91,7 @@ const ItemController = (function(){
 const UIController = (function(){
   const UISelectors = {
     itemList: '#item-list',
+    listItems: '#item-list li',
     addBtn: '.add-btn',
     updateBtn: '.update-btn',
     deleteBtn: '.delete-btn',
@@ -123,6 +141,22 @@ const UIController = (function(){
       document.querySelector(UISelectors.itemNameInput).value = ''
       document.querySelector(UISelectors.itemCaloriesInput).value = ''
     },
+    updateItemDisplay: function(updatedItem){
+      let listItems = document.querySelectorAll(UISelectors.listItems)
+      // Convert node list into array
+      listItems = Array.from(listItems)
+      listItems.forEach(function(li){
+        let liID = li.getAttribute('id')
+        if(liID === `item-${updatedItem.id}`){
+          document.querySelector(`#item-${updatedItem.id}`).innerHTML = `
+          <strong>${updatedItem.name}: </strong> <em>${updatedItem.calories} Calories</em>
+          <a href="#" class="secondary-content">
+            <i class="fa fa-pencil edit-item"></i>
+          </a>
+          `
+        }
+      })
+    },
     hideList: function(){
       document.querySelector(UISelectors.itemList).style.display = 'none'
     },
@@ -156,8 +190,16 @@ const UIController = (function(){
 const App = (function(ItemController, UIController){
   const loadEventListeners = function(){
     const UISelectors = UIController.getSelectors()
+    // Disable submition upon Enter key
+    document.addEventListener('keypress', function(e){
+      if(e.keyCode === 13 || e.which === 13){
+        e.preventDefault()
+        return false
+      }
+    })
     document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit)
     document.querySelector(UISelectors.itemList).addEventListener('click', itemEditClick)
+    document.querySelector(UISelectors.updateBtn).addEventListener('click', itemUpdateSubmit)
   }
   // Add item submit
   const itemAddSubmit = function(e){
@@ -166,7 +208,8 @@ const App = (function(ItemController, UIController){
     // Check for name and calorie input
     if(input.name !== '' && input.calories !== ''){
       // Add food to list
-      const newItem = ItemController.addItem(input.name.value, input.calories.value)
+      let calories = ItemController.processCalorieInput(input.calories.value)
+      const newItem = ItemController.addItem(input.name.value, calories)
       // Add item to UI
       UIController.addListItem(newItem)
       // Clear form fields
@@ -189,6 +232,20 @@ const App = (function(ItemController, UIController){
       ItemController.setCurrentItem(itemToEdit)
       UIController.addItemToForm(itemToEdit)
     }
+    e.preventDefault()
+  }
+
+  const itemUpdateSubmit = function(e){
+    // Update the data
+    let input = UIController.getItemInput()
+    let calories = ItemController.processCalorieInput(input.calories.value)
+    const updatedItem = ItemController.updateItemData(input.name.value, calories)
+    // Update the display
+    UIController.updateItemDisplay(updatedItem)
+    UIController.clearEditState()
+    // Update the total calories
+    const totalCalories = ItemController.getTotalCalories()
+    UIController.showTotalCalories(totalCalories)
     e.preventDefault()
   }
 
